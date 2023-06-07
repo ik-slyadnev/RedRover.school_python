ced79d5549e5db372cdd5fd5e9983c568a7c0190
Elieeeya <ik.slydnev@gmail.com>
2023-06-08 02:55:48 +0300

    add lesson 6

---
+++ lesson_6/6.1.py
@@ -0,0 +1,13 @@
+#Найдите кпопку c текстом "Gold".
+# Попробуйте подобрать как минимум 2 разных XPATH и/или CSS-селекторов
+# http://suninjuly.github.io/xpath_examples
+
+
+
+
+# xpath
+# //button[contains(text(),'Gold')]
+# //div[2]/button[3]
+
+# css
+# body.bg-light:nth-child(2) div:nth-child(2) > button.btn:nth-child(3)
---
+++ lesson_6/6.2.py
@@ -0,0 +1,7 @@
+# Найдите элемент с текстом "Fully charged cat".
+# Чем больше разных XPATH и/или CSS-селекторов сможете подобрать, тем лучше
+# http://suninjuly.github.io/cats.html
+
+
+# //p[contains(text(),'Fully charged cat')]
+# //div[@class="col-sm-4"][5]//p
